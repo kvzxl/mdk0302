7dc1d25e2839e18c938242ef2861aed2468a9103
kvzxl <104169201+kvzxl@users.noreply.github.com>
2023-02-15 15:11:49 +0300

    Add files via upload

---
+++ PractWork6/PractWork5/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PractWork6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pTextBox = new System.Windows.Forms.TextBox();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.qTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pTextBox
+            // 
+            this.pTextBox.Location = new System.Drawing.Point(10, 65);
+            this.pTextBox.Name = "pTextBox";
+            this.pTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pTextBox.TabIndex = 0;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(10, 91);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(347, 20);
+            this.pathTextBox.TabIndex = 1;
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(10, 117);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(347, 23);
+            this.encryptButton.TabIndex = 2;
+            this.encryptButton.Text = "Зашифровать";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(10, 146);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(347, 23);
+            this.decryptButton.TabIndex = 3;
+            this.decryptButton.Text = "Расшифровать";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // qTextBox
+            // 
+            this.qTextBox.Location = new System.Drawing.Point(257, 65);
+            this.qTextBox.Name = "qTextBox";
+            this.qTextBox.Size = new System.Drawing.Size(100, 20);
+            this.qTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "p";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "q";
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(116, 65);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(135, 20);
+            this.selectFileButton.TabIndex = 7;
+            this.selectFileButton.Text = "Выбрать файл";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 203);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.qTextBox);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pTextBox;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.TextBox qTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectFileButton;
+    }
+}
+
---
+++ PractWork6/PractWork5/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PractWork6
+{
+    public partial class Form1 : Form
+    {
+        private int IsSimp(int v)
+        {
+            if (v >= 2)
+            {
+                for (int i = 2; i < v; i++)
+                    if ((v % i) == 0)
+                        throw new Exception();
+            }
+            else
+                throw new Exception();
+            return v;
+        }
+
+        private int VzaimoSimp(int eilFunc)
+        {
+            int e = 0;
+            for (int i = 2; i < eilFunc; i++)
+            {
+                if ((eilFunc % i) != 0)
+                {
+                    e = i;
+                    break;
+                }
+            }
+            if (e == 0)
+                throw new Exception();
+            return e;
+        }
+
+        private byte[] ReadBytes()
+        {
+            return File.ReadAllBytes(pathTextBox.Text);
+        }
+
+        private void WriteFile(string bytCryptoPipto)
+        {
+            var sf = new SaveFileDialog();
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(sf.FileName))
+                {
+                    File.Delete(sf.FileName);
+                }
+                File.WriteAllText(sf.FileName, bytCryptoPipto);
+            }
+        }
+
+
+        private void WriteBytes(byte[] asciiBytes)
+        {
+            var sf = new SaveFileDialog();
+            if (sf.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(sf.FileName))
+                {
+                    File.Delete(sf.FileName);
+                }
+                File.WriteAllBytes(sf.FileName, asciiBytes);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int q = IsSimp(Convert.ToInt32(qTextBox.Text)),
+                    p = IsSimp(Convert.ToInt32(pTextBox.Text)),
+                    n = q * p,
+                    eilFunc = (p - 1) * (q - 1),
+                    openExp = VzaimoSimp(eilFunc);
+                if (n < 127)
+                {
+                    MessageBox.Show("Неккоректные значения, p*q должно быть больше 127", "Ошибка");
+                    return;
+                }
+                if (n > 1681)
+                {
+                    MessageBox.Show("Значение p*q слишком большое", "Ошибка");
+                    return;
+                }
+                double d = 0;
+                while (true)
+                {
+                    if ((d * openExp) % eilFunc == 1)
+                    {
+                        break;
+                    }
+                    d++;
+                }
+
+                int b;
+                int c = 1;
+                byte[] asciiMessage = ReadBytes();
+                string res = "";
+                for (int i = 0; i < asciiMessage.Length; i++)
+                {
+                    b = (int)asciiMessage[i];
+                    for (int j = 0; j < openExp; j++)
+                    {
+                        c = b * c % n;
+                    }
+                    res += c + " ";
+                    c = 1;
+                }
+                res = res.TrimEnd(new char[1] { ' ' });
+                WriteFile(res);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректный ввод");
+            }
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int q = IsSimp(Convert.ToInt32(qTextBox.Text)),
+                    p = IsSimp(Convert.ToInt32(pTextBox.Text)),
+                    n = q * p,
+                    eilFunc = (p - 1) * (q - 1),
+                    openExp = VzaimoSimp(eilFunc);
+                string[] messArray = File.ReadAllText(pathTextBox.Text).Split(' ');
+                double d = 0;
+                int[] intArr = new int[messArray.Length];
+                for (int i = 0; i < messArray.Length; i++)
+                {
+                    intArr[i] = Convert.ToInt32(messArray[i]);
+                }
+                while (true)
+                {
+                    if ((d * openExp) % eilFunc == 1)
+                    {
+                        break;
+                    }
+                    d++;
+                }
+                int[] decryptArr = new int[messArray.Length];
+
+                for (int i = 0; i < intArr.Length; i++)
+                {
+                    int b = intArr[i],
+                       c = 1;
+                    for (int j = 0; j < d; j++)
+                    {
+                        c = (b * c) % n;
+                    }
+                    decryptArr[i] = c;
+                }
+
+                var asciiBytes = new byte[decryptArr.Length];
+                for (int i = 0; i < decryptArr.Length; i++)
+                {
+                    asciiBytes[i] = Convert.ToByte(decryptArr[i]);
+                }
+                WriteBytes(asciiBytes);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Некорректный ввод");
+            }
+        }
+
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            var of = new OpenFileDialog();
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                pathTextBox.Text = of.FileName;
+            }
+        }
+    }
+}
